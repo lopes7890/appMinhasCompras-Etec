99c7d35b6c7aa40377482e4c0dd01def68965361
lopes7890 <lopesallison414@gmail.com>
2025-03-18 14:16:52 -0300

    Adicionando mais excessões para adicionar produtos

--- appMinhasCompras/Models/Produto.cs
+++ appMinhasCompras/Models/Produto.cs
@@ -5,6 +5,8 @@ namespace appMinhasCompras.Models
     public class Produto
     {
         string _descricao;
+        double _quantidade;
+        double _preco;
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
@@ -13,14 +15,32 @@ namespace appMinhasCompras.Models
             {
                 if (value == null)
                 {
-                    throw new Exception("Por favor, preencha a descrição");
+                    throw new Exception("Por favor, preencha a descrição!");
                 }
 
                 _descricao = value;
             }
         }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public double Quantidade { get => _quantidade; set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor, preencha uma quantidade!");
+                }
+
+                _quantidade = value;
+            } 
+        }
+        public double Preco { get => _preco; set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor, preencha um preço!");
+                }
+
+                _preco = value;
+            }
+        }
 
         public double Total { get => Quantidade * Preco; }
     }
